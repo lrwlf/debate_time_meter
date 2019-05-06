52f9b6adbcd54dc77ae7646c178bf0fc261866b4
lrwlf <1139222350@qq.com>
2019-05-06 13:50:14 +0800

    init

---
+++ src/renderer/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'debate_time_meter'
+  }
+</script>
+
+<style>
+  /* CSS */
+  body{
+    background: url('./assets/start.jpg');
+    background-size:cover;
+    background-repeat: no-repeat;
+    
+  }
+</style>
