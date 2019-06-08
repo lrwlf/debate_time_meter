eeeb3d4e4a34f2fb950727ba0389e77bf9c65ae8
lrwlf <1139222350@qq.com>
2019-06-08 23:08:55 +0800

    rm limited

--- src/renderer/pages/start.vue
+++ src/renderer/pages/start.vue
@@ -66,15 +66,15 @@ export default {
   },
   created() {
     this.ipc = require("electron").ipcRenderer;
-    var startDate ='2019-06-10 00:00:00';
-    startDate= startDate.replace(new RegExp("-","gm"),"/");
-    var startDateM = (new Date(startDate)).getTime(); //得到毫秒数
-    var myDate = new Date();
-    console.log(myDate.getTime(),startDateM)
-    if(myDate.getTime()>=startDateM){
-        alert('试用期已过，请联系开发人员！')
-        this.ipc.send('window-close');
-      }
+    // var startDate ='2019-06-10 00:00:00';
+    // startDate= startDate.replace(new RegExp("-","gm"),"/");
+    // var startDateM = (new Date(startDate)).getTime(); //得到毫秒数
+    // var myDate = new Date();
+    // console.log(myDate.getTime(),startDateM)
+    // if(myDate.getTime()>=startDateM){
+    //     alert('试用期已过，请联系开发人员！')
+    //     this.ipc.send('window-close');
+    //   }
     }
 };
 </script>
